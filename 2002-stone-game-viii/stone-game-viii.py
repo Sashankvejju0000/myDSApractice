3d0aafcaa72d8b1b32e9a41437ca01cfed4375a3
Sashankvejju0000 <155641143+Sashankvejju0000@users.noreply.github.com>
2026-08-24 18:25:07 +0530

    Time: 819 ms (68.18%) | Memory: 24.4 MB (18.18%) - LeetSync

---
+++ 2002-stone-game-viii/stone-game-viii.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def stoneGameVIII(self, stones):
+        
+        n = len(stones)
+        
+        
+        pref = [0] * n
+        pref[0] = stones[0]
+        for i in range(1, n):
+            pref[i] = pref[i-1] + stones[i]
+            
+        
+        dp = pref[n-1]
+        
+        
+        for i in range(n-2, 0, -1):
+            dp = max(dp, pref[i] - dp)
+            
+        return dp
